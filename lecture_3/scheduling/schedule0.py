f94faffd563cea6c34f591269d70f2c7673b2c71
muneer alkanafani <muneeralkanafani@gmail.com>
2026-08-21 09:48:13 +0300

    add third example in lecture 3 (schedule)

---
+++ lecture_3/scheduling/schedule0.py
@@ -0,0 +1,68 @@
+"""
+Naive backtracking search without any heuristics or inference.
+"""
+
+DOMAINS = ["Mondey", "Tuesday", "Wednesday"]
+VARIABLES = ["A", "B", "C", "D", "E", "F", "G"]
+CONSTRAINTS = [
+    ("A", "B"),
+    ("A", "C"),
+    ("B", "C"),
+    ("B", "D"),
+    ("B", "E"),
+    ("C", "E"),
+    ("C", "F"),
+    ("D", "E"),
+    ("E", "F"),
+    ("E", "G"),
+    ("F", "G")
+]
+
+def backtrack(assignment):
+    """Runs backtracking search to find an assignment."""
+
+    # Check if assignment is complete
+    if len(assignment) == len(VARIABLES):
+        return assignment
+
+    # Try a new variable
+    var = select_unassigned_variable(assignment)
+    for value in DOMAINS:
+        new_assignment = assignment.copy()
+        new_assignment[var] = value
+        if consistent(new_assignment):
+            result = backtrack(new_assignment)
+            if result is not None:
+                return result
+    return None
+
+
+def select_unassigned_variable(assignment):
+    """chooses a variable not yet assigned, in order"""
+
+    for variable in VARIABLES:
+        if variable not in assignment:
+            return variable
+    
+    return None
+
+def consistent(assignment):
+    """Checks to see if an assignment is consistent."""
+
+    # loop for all the constraints
+    for (x, y) in CONSTRAINTS:
+
+        # Only consider arcs where both are assigned
+        if x not in assignment or y not in assignment:
+            continue
+
+        # If both have same value, then not consistent
+        if assignment[x] == assignment[y]:
+            return False
+        
+    # If nothing inconsistent, then assignment is consistent
+    return True
+
+if __name__ == "__main__":
+    solution = backtrack(dict())
+    print(solution)
