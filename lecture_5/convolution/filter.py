28df2325db6c6cdf2b8b0d15a919ca077067758f
muneer alkanafani <muneeralkanafani@gmail.com>
2026-09-17 12:20:42 +0300

    add second example in lecture 5 (convolution)

---
+++ lecture_5/convolution/filter.py
@@ -0,0 +1,26 @@
+import math
+import sys
+import os
+from PIL import Image, ImageFilter
+
+# Ensure correct usage
+if len(sys.argv) != 2:
+    sys.exit("Usage: python filter.py filename")
+
+# Open image
+image = Image.open(sys.argv[1]).convert("RGB")
+
+# Filter image according to edge detection kernel
+filtered = image.filter(ImageFilter.Kernel(
+    size=(3, 3),
+    kernel=[-1, -1, -1, -1, 8, -1, -1, -1, -1],
+    scale=1
+))
+
+# Show resulting image
+filtered.show()
+
+# Save the image
+infile = sys.argv[1]
+root, ext = os.path.splitext(infile)
+filtered.save(f"{root}_filtered{ext}", format="PNG")
